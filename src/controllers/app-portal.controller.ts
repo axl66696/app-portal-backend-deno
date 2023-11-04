033900c0be072c09f6a7970a0637190e76d49d63
Tommy Huang <axl66696@gmail.com>
2023-11-04 15:44:39 +0800

    init＋整理code

--- src/controllers/app-portal.controller.ts
+++ src/controllers/app-portal.controller.ts
@@ -2,9 +2,9 @@ import { AppPortalService } from '../services/mod.ts';
 import { Controller, inject, Replier, Subscriber } from '@his/base/controller-base/mod.ts';
 import { JsMsg, JSONCodec, Msg } from 'https://deno.land/x/nats@v1.17.0/src/mod.ts';
 import { MongoBaseService } from '@his/base/mongo-base/mod.ts';
-import { nanoid } from 'npm:nanoid';
+// import { uuid } from 'crypto.randomUUID';
 import { Coding } from '@his-base/datatypes';
-import { AppNews } from '@his-viewmodel/appPortal';
+import { AppNews } from '@his/viewmodel/app-portal/index.js';
 
 @Controller('appPortal')
 export class AppPortalController {
@@ -18,57 +18,63 @@ export class AppPortalController {
    */
   @Replier('appNews.find')
   async getAppNewsList(message: Msg, payload: string) {
+    try {
+      const appNews = await this.#appPortalService.getAppNewsList(payload);
+      message.respond(this.#codec.encode(appNews));
+    } catch {
+      message.respond(this.#codec.encode([]));
+    }
     
-    const appNews = await this.#appPortalService.getAppNewsList(payload);
-    message.respond(this.#codec.encode(appNews));
   }
 
   /** 新增最新消息
    *  新增最新消息後，將最新消息發送給相關使用者
    */
   @Subscriber('appNews.add')
-  async pubUserNews(message: JsMsg, payload: any) {
+  async insertAppNews(message: JsMsg, payload: AppNews) {
 
     try {
      
-      const _id = nanoid();
-      const tmpNews = {
-        '_id': _id,
-        'appStore_ids': payload.appStore_ids,
-        'level': payload.level,
-        'title': payload.title,
-        'url': payload.url,
-        'sendUser': payload.sendUser,
-        'sendTime': new Date(payload.sendTime),
-        'expiredTime': new Date(payload.expiredTime),
-        'updatedBy': payload.updatedBy,
-        'updatedAt': new Date(payload.updatedAt),
-      };
+      // const _id = nanoid();
+      // const tmpNews = {
+      //   '_id': _id,
+      //   'appStore_ids': payload.appStore_ids,
+      //   'level': payload.level,
+      //   'title': payload.title,
+      //   'url': payload.url,
+      //   'sendUser': payload.sendUser,
+      //   'sendTime': new Date(payload.sendTime),
+      //   'expiredTime': new Date(payload.expiredTime),
+      //   'updatedBy': payload.updatedBy,
+      //   'updatedAt': new Date(payload.updatedAt),
+      // };
 
-      let userIds: any = [];
-      if (tmpNews.appStore_ids.length > 1) {
+      this.#appPortalService.addAppNews(payload);
+      // let userIds: any = [];
+      // if (tmpNews.appStore_ids.length > 1) {
 
-        const ids: object[] = [];
-        const appStoreFilter = { $or: ids };
-        tmpNews.appStore_ids.forEach((x: string) => {
-          ids.push({ 'appStore_id': x });
-        });
-        userIds = await this.#appPortalService.getUserIds(appStoreFilter);
-      } else if (tmpNews.appStore_ids.length === 1) {
+      //   const ids: object[] = [];
+      //   const appStoreFilter = { $or: ids };
+      //   tmpNews.appStore_ids.forEach((x: string) => {
+      //     ids.push({ 'appStore_id': x });
+      //   });
+      //   userIds = await this.#appPortalService.getUserIds(appStoreFilter);
+      // } else if (tmpNews.appStore_ids.length === 1) {
         
-        const appStoreFilter = { 'appStore_id': tmpNews.appStore_ids[0] };
-        userIds = await this.#appPortalService.getUserIds(appStoreFilter);
-      } else userIds = await this.#appPortalService.getUserIds({});
+      //   const appStoreFilter = { 'appStore_id': tmpNews.appStore_ids[0] };
+      //   userIds = await this.#appPortalService.getUserIds(appStoreFilter);
+      // } else userIds = await this.#appPortalService.getUserIds({});
       
-      await this.#appPortalService.insertAppNews(tmpNews);
-      userIds.forEach(async (user: Coding) => {
-        const payload = {
-          appNews_id: tmpNews._id,
-          user: user,
-          readTime: new Date('2999-12-31T23:59:59.000Z'),
-        };
-        await this.#appPortalService.pubUserNews(user, payload);
-      });
+      // await this.#appPortalService.insertAppNews(tmpNews);
+      await this.#appPortalService.pubUserNews(payload);
+      // userIds.forEach(async (user: Coding) => {
+      //   const payload = {
+      //     appNews_id: tmpNews._id,
+      //     user: user,
+      //     readTime: new Date('2999-12-31T23:59:59.000Z'),
+      //   };
+      //   await this.#appPortalService.pubUserNews(user, payload);
+      // });
       message.ack();
     } catch (error) {
       
@@ -85,16 +91,16 @@ export class AppPortalController {
     
     try {
 
-      const userNews_id = nanoid();
-      const tmpNews = {
-        '_id': userNews_id,
-        'appNews_id': payload.appNews_id,
-        'user': payload.user,
-        'readTime': new Date(payload.readTime),
-        'updatedBy': payload.user,
-        'updatedAt': new Date(),
-      };
-      await this.#appPortalService.insertUserNews(tmpNews);
+      // const userNews_id = nanoid();
+      // const tmpNews = {
+      //   '_id': userNews_id,
+      //   'appNews_id': payload.appNews_id,
+      //   'user': payload.user,
+      //   'readTime': new Date(payload.readTime),
+      //   'updatedBy': payload.user,
+      //   'updatedAt': new Date(),
+      // };
+      await this.#appPortalService.insertUserNews(payload);
       message.ack();
     } catch (error) {
 
@@ -107,78 +113,84 @@ export class AppPortalController {
    *  取得newsContent
    */
   @Replier('news.newsContent')
-  async getMyAppNews(message: Msg, payload: any) {
+  async getMyAppNews(message: Msg, _id: string) {
 
-    const pipeline = [
-      /** 第一階段：篩選符合條件的 AppNews 數據 */
-      {
-        $match: {
-          '_id': payload,
-        },
-      },
-      /** 第二階段
-       * 從 AppNews 集合中關聯 UserNews 數據
-       */
-      {
-        $lookup: {
-          from: 'UserNews', // 關聯的collection名稱
-          localField: '_id', // AppNews 集合中的字段，用於關聯
-          foreignField: 'appNews_id', // UserNews 集合中的字段，用於關聯
-          as: 'userNewsData', // 輸出字段的别名
-        },
-      },
-      /** 第三階段：將相關數據進行重構，生成合併後的文檔 */
-      {
-        $unwind: {
-          path: '$userNewsData',
-          preserveNullAndEmptyArrays: true,
-        },
-      },
-      /** 第四階段：輸出所需的欄位 */
-      {
-        $project: {
-          code: '$userNewsData.code',
-          appNews_id: '$userNewsData._id',
-          appStore_id: 1,
-          level: 1,
-          url: 1,
-          title: 1,
-          sendUser: 1,
-          sendTime: 1,
-          expiredTime: 1,
-          readTime: '$userNewsData.readTime',
-        },
-      },
-    ];
-    const myAppNews = await this.#appPortalService.getMyAppNews(pipeline);
-    if (myAppNews) {
-
+    // const pipeline = [
+    //   /** 第一階段：篩選符合條件的 AppNews 數據 */
+    //   {
+    //     $match: {
+    //       '_id': payload,
+    //     },
+    //   },
+    //   /** 第二階段
+    //    * 從 AppNews 集合中關聯 UserNews 數據
+    //    */
+    //   {
+    //     $lookup: {
+    //       from: 'UserNews', // 關聯的collection名稱
+    //       localField: '_id', // AppNews 集合中的字段，用於關聯
+    //       foreignField: 'appNews_id', // UserNews 集合中的字段，用於關聯
+    //       as: 'userNewsData', // 輸出字段的别名
+    //     },
+    //   },
+    //   /** 第三階段：將相關數據進行重構，生成合併後的文檔 */
+    //   {
+    //     $unwind: {
+    //       path: '$userNewsData',
+    //       preserveNullAndEmptyArrays: true,
+    //     },
+    //   },
+    //   /** 第四階段：輸出所需的欄位 */
+    //   {
+    //     $project: {
+    //       code: '$userNewsData.code',
+    //       appNews_id: '$userNewsData._id',
+    //       appStore_id: 1,
+    //       level: 1,
+    //       url: 1,
+    //       title: 1,
+    //       sendUser: 1,
+    //       sendTime: 1,
+    //       expiredTime: 1,
+    //       readTime: '$userNewsData.readTime',
+    //     },
+    //   },
+    // ];
+    try {
+      const myAppNews = await this.#appPortalService.getMyAppNews(_id);
       message.respond(this.#codec.encode(myAppNews));
+    } catch {
+      message.respond(this.#codec.encode([]));
     }
+    
+    // if (myAppNews) {
+
+    //   message.respond(this.#codec.encode(myAppNews));
+    // }
   }
 
   /** 修改最新消息
    *  以修改updatedAt的方式修改最新消息
    */
   @Subscriber('appNews.modify')
-  async modifyAppNews(message: JsMsg, payload: AppNews) {
+  async modifyAppNews(message: JsMsg, appNews: AppNews) {
 
     try {
 
       const nowDate = new Date();
-      const tmpNews = {
-        '_id': payload._id,
-        'appStore_ids': payload.appStore_ids,
-        'level': payload.level,
-        'title': payload.title,
-        'url': payload.url,
-        'sendUser': payload.sendUser,
-        'sendTime': new Date(payload.sendTime),
-        'expiredTime': new Date(payload.expiredTime),
-        'updatedBy': payload.updatedBy,
-        'updatedAt': nowDate,
-      };
-      await this.#appPortalService.modiftAppNews(tmpNews);
+    //   const tmpNews = {
+    //     '_id': payload._id,
+    //     'appStore_ids': payload.appStore_ids,
+    //     'level': payload.level,
+    //     'title': payload.title,
+    //     'url': payload.url,
+    //     'sendUser': payload.sendUser,
+    //     'sendTime': new Date(payload.sendTime),
+    //     'expiredTime': new Date(payload.expiredTime),
+    //     'updatedBy': payload.updatedBy,
+    //     'updatedAt': nowDate,
+    //   };
+      await this.#appPortalService.modiftAppNews(appNews);
       message.ack();
     } catch (error) {
 
@@ -210,7 +222,12 @@ export class AppPortalController {
   @Replier('appStore.find')
   async getAppStores(message: Msg, payload: any) {
 
-    const appStores = await this.#appPortalService.getAppStores();
-    message.respond(this.#codec.encode(appStores));
+    try {
+      const appStores = await this.#appPortalService.getAppStores();
+      message.respond(this.#codec.encode(appStores));
+    } catch {
+      message.respond(this.#codec.encode([]));
+    }
+    
   }
 }
--- src/services/app-portal.service.ts
+++ src/services/app-portal.service.ts
@@ -2,12 +2,15 @@ import { inject } from '@his/base/controller-base/mod.ts';
 import { MongoBaseService } from '@his/base/mongo-base/mod.ts';
 import { JetStreamService } from '@his/base/jetstream/mod.ts';
 import { Coding } from '@his-base/datatypes';
-import { AppNews, UserNews} from '@his-viewmodel/appPortal';
+import { AppNews, UserNews } from '@his/viewmodel/app-portal/index.js';
+import { nanoid } from 'npm:nanoid'
 
 export class AppPortalService {
   
   mongoDB = inject(MongoBaseService);
   jetStreamService = inject(JetStreamService);
+  subjectBase = 'appPortal.appPortal.userNews.';
+
 
   /** 取得最新消息清單
    *  使用find()拿到最新消息清單後回傳
@@ -27,14 +30,41 @@ export class AppPortalService {
     return await db.collection('AppStore').find().toArray();
   }
 
+  async addAppNews(appNews: AppNews) {
+    await using db = await this.mongoDB.connect();
+    const _id = nanoid();
+    appNews._id = _id; 
+    
+    await db.collection('AppNews').insertOne(appNews);
+  }
+
+  async getUserIds(appStore_ids) {
+
+    await using db = await this.mongoDB.connect();
+    let user_ids: any = [];
+    if (appStore_ids.length > 1) {
+
+      const ids: object[] = [];
+      const appStoreFilter = { $or: ids };
+      appStore_ids.forEach((x: string) => {
+        ids.push({ 'appStore_id': x });
+      });
+      return await db.collection('UserAppStore').distinct('user', appStoreFilter);
+    } else if (appStore_ids.length === 1) {
+      
+      const appStoreFilter = { 'appStore_id': appStore_ids[0] };
+      return await db.collection('UserAppStore').distinct('user', appStoreFilter);
+    } else return await db.collection('UserAppStore').distinct('user', {});
+  }
+  
   /** 取得選定的appStore中所有使用者id
    *  使用distinct()過濾重複的使用者id後回傳
    */
-  async getUserIds(appStoreFilter: object) {
+  // async gtUserIds(appStoreFilter: object) {
 
-    await using db = await this.mongoDB.connect();
-    return await db.collection('UserAppStore').distinct('user', appStoreFilter);
-  }
+  //   await using db = await this.mongoDB.connect();
+  //   return await db.collection('UserAppStore').distinct('user', appStoreFilter);
+  // }
 
   /** 新增最新消息到資料庫
    *  使用insertOne()將appNews存入資料庫
@@ -48,12 +78,20 @@ export class AppPortalService {
   /** 發送userNews資訊到nats
    *  publish userNews
    */
-  async pubUserNews(user: Coding, payload: object) {
+  async pubUserNews(appNews: AppNews) {
 
-    await this.jetStreamService.publish(
-      `appPortal.appPortal.userNews.${user.code}`,
-      payload,
-    );
+    const user_ids = await this.getUserIds(appNews.appStore_ids);
+    user_ids.forEach(async (user: Coding) => {
+      const payload = {
+        appNews_id: appNews._id,
+        user: user,
+        readTime: new Date('2999-12-31T23:59:59.000Z'),
+      };
+      await this.jetStreamService.publish(
+        `appPortal.appPortal.userNews.${user.code}`,
+        payload,
+      );
+    });
   }
 
   /** 接收userNews並存入資料庫
@@ -61,6 +99,16 @@ export class AppPortalService {
    */
   async insertUserNews(userNews: UserNews) {
 
+    const userNews_id = nanoid();
+      const tmpNews = {
+        '_id': userNews_id,
+        'appNews_id': userNews.appNews_id,
+        'user': userNews.user,
+        'readTime': new Date(userNews.readTime),
+        'updatedBy': userNews.user,
+        'updatedAt': new Date(),
+      };
+    
     await using db = await this.mongoDB.connect();
     await db.collection('UserNews').insertOne(userNews);
   }
@@ -68,7 +116,49 @@ export class AppPortalService {
   /** 取得新增的最新消息內容
    *  使用aggregate()取得新增的最新消息內容
    */
-  async getMyAppNews(pipeline: Object[]) {
+  async getMyAppNews(_id) {
+
+    const pipeline = [
+      /** 第一階段：篩選符合條件的 AppNews 數據 */
+      {
+        $match: {
+          '_id': _id,
+        },
+      },
+      /** 第二階段
+       * 從 AppNews 集合中關聯 UserNews 數據
+       */
+      {
+        $lookup: {
+          from: 'UserNews', // 關聯的collection名稱
+          localField: '_id', // AppNews 集合中的字段，用於關聯
+          foreignField: 'appNews_id', // UserNews 集合中的字段，用於關聯
+          as: 'userNewsData', // 輸出字段的别名
+        },
+      },
+      /** 第三階段：將相關數據進行重構，生成合併後的文檔 */
+      {
+        $unwind: {
+          path: '$userNewsData',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+      /** 第四階段：輸出所需的欄位 */
+      {
+        $project: {
+          code: '$userNewsData.code',
+          appNews_id: '$userNewsData._id',
+          appStore_id: 1,
+          level: 1,
+          url: 1,
+          title: 1,
+          sendUser: 1,
+          sendTime: 1,
+          expiredTime: 1,
+          readTime: '$userNewsData.readTime',
+        },
+      },
+    ];
 
     await using db = await this.mongoDB.connect();
     return await db.collection('AppNews').aggregate(pipeline).toArray();
@@ -80,6 +170,7 @@ export class AppPortalService {
   async modiftAppNews(appNews: AppNews) {
 
     await using db = await this.mongoDB.connect();
+  
     await db.collection('AppNews').updateOne(
       { _id: appNews._id },
       {
